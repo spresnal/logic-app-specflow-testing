cbebcbe5fc060410831af353645d34f6f0aa00cd
Sam Presnal <samgpresnal@gmail.com>
2019-11-23 12:35:59 -0500

    initial commit

---
+++ TimeForCoffee.Tests.Feature/EnvVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeForCoffee.Tests.Feature
+{
+    public static class EnvVariables
+    {
+        public static string AzureClientId = Environment.GetEnvironmentVariable("AzureClientId");
+        public static string AzureClientSecret = Environment.GetEnvironmentVariable("AzureClientSecret");
+        public static string AzureTenantId = Environment.GetEnvironmentVariable("AzureTenantId");
+        public static string SubscriptionId = Environment.GetEnvironmentVariable("SubscriptionId");
+        public static string ResourceGroup = Environment.GetEnvironmentVariable("ResourceGroup");
+    }
+}
---
+++ TimeForCoffee.Tests.Feature/TimeForCoffee.feature
@@ -0,0 +1,9 @@
+﻿Feature: TimeForCoffee
+	In order to drink the right amount of coffee
+	As a developer
+	I want to be told how many cups to drink
+
+Scenario: TimeForCoffee noon test
+	Given I send a POST request to 'url' with the body
+	| currentTime |
+	| 12:00       |
